a8e49882e9ff3e79dbc8fe506228a6de2afbb517
Wouter Deconinck <wdconinc@gmail.com>
2026-09-23 15:31:39 -0500

    Switch apptainer to singularity

--- bin/start_redis.sh
+++ bin/start_redis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Start a Redis server using Apptainer
+# Start a Redis server using Singularity
 
 PORT=${1:-6379}
 DATADIR=$(pwd)/.redis_data
@@ -9,7 +9,7 @@ echo "Starting Redis on port $PORT..."
 echo "Data directory: $DATADIR"
 
 # Run redis-server in the background using the official docker image
-apptainer run --bind "$DATADIR:/data" docker://redis:alpine redis-server --port $PORT --dir /data --save 60 1 &
+singularity run --bind "$DATADIR:/data" docker://redis:alpine redis-server --port $PORT --dir /data --save 60 1 &
 REDIS_PID=$!
 echo $REDIS_PID > .redis.pid
 
